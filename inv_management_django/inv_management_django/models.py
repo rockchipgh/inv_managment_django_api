aeef80bb2d6abb481179dd06d898ec35378bed41
Rohit P S <rohithp61@gmail.com>
2024-10-02 19:15:15 +0530

    restructed directory

--- inv_management_django/inv_management_django/models.py
+++ inv_management_django/inv_management_django/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Item(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
